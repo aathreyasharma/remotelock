f83e92205d1870af0452a30286eae266a7ade0c5
Aathreya Sharma <aathreya.sharma@outlook.com>
2022-01-09 16:26:24 +0530

    Adds Parsing service

--- app/people_controller.rb
+++ app/people_controller.rb
@@ -1,9 +1,20 @@
+require_relative 'services/parse_service'
 class PeopleController
   def initialize(params)
     @params = params
   end
 
   def normalize
+    arr = []
+    @params.keys.each do |key|
+      if key.to_s.include?('format') && !@params[key].nil?
+        arr << ParseService.new(@params[key], key).call
+      end
+    end
+    arr.flatten!
+    arr.sort_by!{|elem| [elem.send(@params[:order]) ? 1 : 0, elem.send(@params[:order])] } unless @params[:order].nil?
+    arr.map!(&:stringify) unless arr.empty?
+    arr
   end
 
   private
